0c27beeba1ac3df33252da0f2672da86fb4e539b
kevanjitsingh1 <singhkevanjit@gmail.com>
2024-10-28 23:40:03 -0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace NUnitDemonstration
+{
+    public static class Triangle
+    {
+        public static string ValidTriangle(int firstAngle, int secondAngle, int thirdAngle)
+        {
+            
+            // Check if the sum of the angles is equal to 180
+            if ((firstAngle + secondAngle + thirdAngle) == 180)
+            {
+                return "The triangle is valid.";
+            }
+            else
+            {
+                return "The triangle is not valid.";
+            }
+        }
+    }
+}
